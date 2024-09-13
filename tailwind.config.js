b9a7a100c162d5596f51d2df518cf151ff997112
Josh-Varney <127770606+Josh-Varney@users.noreply.github.com>
2024-09-13 08:35:39 +0100

    frontend

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import plugin from 'tailwindcss/plugin'
+
+export const plugins = [
+    plugin(function ({ addUtilities, addComponents, e, config }) {
+        // Add your custom styles here
+    }),
+]
