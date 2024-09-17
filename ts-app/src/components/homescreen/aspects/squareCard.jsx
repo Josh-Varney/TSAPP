281ebc83bcea362765c4d9a2f8171f79665835cc
josh2 <jrv123756@gmail.com>
2024-09-17 09:52:55 +0100

    Dashboard Layout Complete

--- ts-app/src/components/homescreen/aspects/squareCard.jsx
+++ ts-app/src/components/homescreen/aspects/squareCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ({ backgroundImage, headerText, textArea, spanText, icon, link }) => {
+const Card = ({ backgroundImage, headerText, textArea, spanText, icon: Icon, link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" className="p-20 w-1/3 rounded overflow-hidden justify-items-center shadow-lg hover:shadow-2xl">
       <div className="relative w-full">
         <img className="w-full h-auto" src={backgroundImage} alt="Background" />
         <div className="absolute bottom-4 left-0 flex items-center justify-center w-20 h-20 bg-gray-200 border border-gray-300 rounded ml-10">
-          {icon}
+          {Icon && <Icon className="w-10 h-10"/>}
         </div>
       </div>
       <div className="px-6 py-4">
--- ts-app/src/components/homescreen/home.jsx
+++ ts-app/src/components/homescreen/home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import HomeNavbar from './aspects/nav';
 import Card from './aspects/squareCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { HiMiniMagnifyingGlass, HiUserGroup } from "react-icons/hi2";
+import { MdEmojiEvents } from "react-icons/md";
+import { PiCalendarBold } from "react-icons/pi";
 
 const HomeScreen = () => {
     return (
@@ -16,13 +17,14 @@ const HomeScreen = () => {
                     headerText="Book a Professional"
                     textArea="Let's find an experienced teacher that meet's your requirements"
                     spanText="1-to-1 Lessons"
-                    icon={<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />}
+                    icon={HiMiniMagnifyingGlass}
                 />
                 <Card 
                     backgroundImage="https://via.placeholder.com/600x400"
                     headerText="Join a Group Session"
                     textArea="Share an experienced professional amongst multiple students"
                     spanText="Up to 2-to-8 Lessons"
+                    icon={HiUserGroup}
                 />
             </div>
             <div className='flex flex-row justify-items-center justify-center mt-10 space-x-10'>
@@ -31,12 +33,14 @@ const HomeScreen = () => {
                     headerText="Check out our Events"
                     textArea="Stay up-to-date with continuous large group events throughout the year"
                     spanText="Examination Events or Meet Our Team?"
+                    icon={MdEmojiEvents}
                 />
                 <Card 
                     backgroundImage="https://via.placeholder.com/600x400"
                     headerText="Check your Timetable"
                     textArea="The busier the schedule the more there is to learn here!"
                     spanText="Let's set and maintain our schedule"
+                    icon={PiCalendarBold}
                 />
             </div>
         </div>
